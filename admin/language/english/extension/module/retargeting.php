90d3e1e109f8759e97522db93e63fe5e1a13dd4f
pelucaroltheo@gmail.com <pelucaroltheo@gmail.com>
2017-01-23 17:17:46 +0200

    add saveOrder API & clickImage functionality

---
+++ admin/language/english/extension/module/retargeting.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * LANGUAGE FILE for ADMIN VIEW
+ * English
+ * catalog/language/english/extension/module
+ * retargeting.php
+ *
+ * MODULE: Retargeting
+ */
+
+// Heading
+$_['heading_title']     = 'Retargeting Module for OpenCart 2.3.x';
+$_['text_edit'] = 'Retargeting Module for OpenCart 2.3.x';
+
+// Text
+$_['text_module']        = 'Modules';
+$_['text_success']       = '<strong>You have successfully updated Retargeting module!</strong>';
+
+$_['api_key']            = 'Retargeting API Key:';
+$_['api_secret']          = 'Retargeting API Secret:';
+
+$_['text_content_top']    = 'Content Top';
+$_['text_content_bottom'] = 'Content Bottom';
+$_['text_column_left']    = 'Column Left';
+$_['text_column_right']   = 'Column Right';
+
+
+$_['entry_code']        = 'Retargeting Module Code:';
+$_['entry_layout']      = 'Layout:';
+$_['entry_position']    = 'Position:';
+$_['entry_status']      = 'Status:';
+$_['entry_sort_order']  = 'Sort Order:';
+
+$_['button_remove'] = 'Remove';
+$_['button_add_module'] = 'Add Layout';
+$_['text_enabled'] = 'Enabled';
+$_['text_disabled'] = 'Disabled';
+
+// Error
+$_['error_permission']  = 'You do not have permission to modify the Retargeting module! Check System - Users - User Groups!';
+$_['error_code']        = 'API Key is required for the module to work!';
+
+
+// ONLY FOR OPENCART 2.x
+$_['text_edit'] = 'Retargeting Module for OpenCart 2.3.x';
+$_['entry_apikey'] = 'Tracking API Key: ';
+$_['entry_token'] = 'REST API Key: ';
+$_['error_token_required'] = '<strong>You MUST enter the REST API Key for a fully functional module.</strong>';
+$_['error_apikey_required'] = '<strong>You MUST enter the Tracking API Key for a fully functional module.</strong>';
+
+?>
