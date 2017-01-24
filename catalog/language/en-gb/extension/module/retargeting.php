f8458e0d12e694f773fba4d3747d5d25bcdcbfa1
pelucaroltheo@gmail.com <pelucaroltheo@gmail.com>
2017-01-24 14:54:45 +0200

    change languages folder structure

---
+++ catalog/language/en-gb/extension/module/retargeting.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * LANGUAGE FILE for CATALOG / STORE VIEW
+ * English
+ * catalog/language/en-gb/extention/module
+ * retargeting.php
+ *
+ * MODULE: Retargeting
+ */
+
+
+// Heading 
+$_['heading_title']  = 'Retargeting Module';
